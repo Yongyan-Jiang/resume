dd8bd07ea1fddb2b97d4d1b74fa47529bab8a45b
Yongyan-Jiang <2810312487@qq.com>
2019-05-03 21:12:32 +0800

    creat server.js

---
+++ projects/node-server/README.md
@@ -0,0 +1,2 @@
+# node-server
+nodejs 实现能处理路由，支持静态目录的后端服务器
---
+++ projects/node-server/server.js
@@ -0,0 +1,34 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+
+
+http.createServer(function(req, res){
+  
+  var pathObj = url.parse(req.url, true)
+  console.log(pathObj)
+
+  switch (pathObj.pathname) {
+    case '/getWeather':
+      var ret
+      if(pathObj.query.city == 'beijing'){
+        ret = {
+          city: 'beijing',
+          weather: '晴天'
+        }
+      }else{
+        ret = {
+          city: pathObj.query.city,
+          weather: '不知道'
+        }
+      }
+      res.end(JSON.stringify(ret))
+      break;
+    case '/':
+      res.end( fs.readFileSync(__dirname + '/sample/test.html') )
+      break;
+    default:
+      res.end( fs.readFileSync(__dirname + '/sample' + pathObj.pathname) )
+  }
+
+}).listen(8080)
