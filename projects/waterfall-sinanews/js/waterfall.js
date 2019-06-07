2c8614dcd73e0b5ebfc5fe2f0355a4c4bab27e92
Yongyan-Jiang <2810312487@qq.com>
2019-06-07 09:48:19 +0800

    add

--- projects/waterfall-sinanews/js/waterfall.js
+++ projects/waterfall-sinanews/js/waterfall.js
@@ -8,7 +8,8 @@ var WaterFall = {
   },
 
   bind: function(){
-    var me = this;
+    var me = this,
+        timer = null;
     $(window).on('resize', function(){
       if(timer){
         clearTimeout(timer)
@@ -19,15 +20,38 @@ var WaterFall = {
     })
   },
 
-  start: function(){
+  start: function($nodes){
     var me = this;
+    this.$items = this.$ct.find('.item');
+    if(this.$items.length ===0) return;
+    this.itemWidth = this.$items.outerWidth(true);
+    this.$ct.width('auto');
     this.colNum = Math.floor( this.$ct.width() / this.itemWidth );
-    for(var i=0; i<this.colNum; i++){
-      this.arrColHeight[i] = 0;
+    this.$ct.width(this.itemWidth*this.colNum);
+    if(this.arrColHeight.length === 0 || !$nodes){
+      this.arrColHeight = [];
+      for(var i=0; i<this.colNum; i++){
+        this.arrColHeight[i] = 0;
+      }	
     }
-    this.$items.each(function(){
-      me.placeItem( $(this) );
-    });
+    if($nodes){
+      //console.log(this.arrColHeight.length)
+      $nodes.each(function(){
+        var $item = $(this);
+        $item.find('img').on('load', function(){
+          me.placeItem( $item );
+          me.$ct.height( Math.max.apply(null, me.arrColHeight) );
+        })
+      });	
+    }else{
+      this.$items.each(function(){
+        var $item = $(this);
+        me.placeItem( $item );
+      });
+      console.log(me.arrColHeight);
+      me.$ct.height( Math.max.apply(null, me.arrColHeight) );	
+    }
+    
   },
 
   placeItem: function( $el ) {
